83386abb66eee030f4cd5c7ce244f8d5ce45bdfc
BFT\a.denezhkin <a.denezhkin@bftcom.com>
2021-11-13 23:33:09 +0400

    Calculator Model

--- src/main/java/ru/aden/springmvc/controllers/FirstController.java
+++ src/main/java/ru/aden/springmvc/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.aden.springmvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,8 +14,10 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false)String name,
-                            @RequestParam(value = "surname", required = false)String surname){
-        System.out.println(name + " " + surname);
+                            @RequestParam(value = "surname", required = false)String surname,
+                            Model model){
+        model.addAttribute("message", "Hello " + name + " " + surname);
+        //  System.out.println(name + " " + surname);
         return "first/hello";
     }
 
