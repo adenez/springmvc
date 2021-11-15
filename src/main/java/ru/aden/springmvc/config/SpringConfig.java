4b79489c47fd27595405d76bd23ead3e945a73f1
BFT\a.denezhkin <a.denezhkin@bftcom.com>
2021-11-15 10:41:35 +0400

    Removed DB login and password from Config Java Class

--- src/main/java/ru/aden/springmvc/config/SpringConfig.java
+++ src/main/java/ru/aden/springmvc/config/SpringConfig.java
@@ -15,6 +15,11 @@ import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @Configuration
 @ComponentScan("ru.aden.springmvc")
@@ -53,11 +58,23 @@ public class SpringConfig implements WebMvcConfigurer {
 
     @Bean
     public DataSource dataSource(){
+        Properties prop = new Properties();
+        try (InputStream in = getClass().getClassLoader().getResourceAsStream("database.properties")){
+            prop.load(in);
+        }
+        catch (Exception e) {
+            System.out.println("Error:");
+            e.printStackTrace();
+        }
+        String url = prop.getProperty("jdbc.url");
+        String username = prop.getProperty("jdbc.username");
+        String password = prop.getProperty("jdbc.password");
+
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/firstDB");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("postgres");
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
 
         return dataSource;
     }
